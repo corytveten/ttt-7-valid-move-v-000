14343201eed4263be2175578383e22378b092fd9
corytveten <corytveten@gmail.com>
2018-04-09 16:41:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index >= 0 && index < 9
     if position_taken?(board, index) == false
       return true
-    elsif position_taken?(board, index) == false
+    elsif position_taken?(board, index) == true
       return false
     end
   else
